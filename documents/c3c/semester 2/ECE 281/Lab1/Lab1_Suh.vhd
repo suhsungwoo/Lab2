c225ee620d0e36d69d5b8dccdcaa48751fb5bd7e
Sung.Suh <C16Sung.Suh@usafa.edu>
2014-01-23 01:15:43 -0700

    step 14

---
+++ documents/c3c/semester 2/ECE 281/Lab1/Lab1_Suh.vhd
@@ -0,0 +1,47 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    00:51:10 01/23/2014 
+-- Design Name: 
+-- Module Name:    Lab1_Suh - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Lab1_Suh is
+    Port ( A : in  STD_LOGIC;
+           B : in  STD_LOGIC;
+           C : in  STD_LOGIC;
+           X : out  STD_LOGIC;
+           Y : out  STD_LOGIC;
+           Z : out  STD_LOGIC);
+end Lab1_Suh;
+
+architecture Behavioral of Lab1_Suh is
+
+begin
+
+
+end Behavioral;
+
